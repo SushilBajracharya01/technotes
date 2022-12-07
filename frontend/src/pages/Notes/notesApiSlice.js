5c57e9797e2970b52187f693948ef1e087044195
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2022-12-07 19:55:06 +0545

    feat: redux setup

---
+++ frontend/src/pages/Notes/notesApiSlice.js
@@ -0,0 +1,55 @@
+import {
+    createSelector,
+    createEntityAdapter
+} from '@reduxjs/toolkit';
+
+import { apiSlice } from '../../app/api/apiSlice';
+
+const notesAdapter = createEntityAdapter({});
+
+const initialState = notesAdapter.getInitialState();
+
+export const notesApliSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getNotes: builder.query({
+            query: () => '/notes',
+            validateStatus: (response, result) => {
+                return response.status === 200 && !result.isError
+            },
+            keepUnusedDataFor: 5,
+            transformResponse: responseData => {
+                const loadedNotes = responseData.map(note => {
+                    note.id = note._id;
+                    return note;
+                });
+                return notesAdapter.setAll(initialState, loadedNotes)
+            },
+            providesTags: (result, error, arg) => {
+                if (result?.ids) {
+                    return [
+                        { type: 'Note', id: 'LIST' },
+                        ...result.ids.map(id => ({ type: "Note", id }))
+                    ]
+                } else return [{ type: 'Note', id: 'LIST' }]
+            }
+        })
+    })
+});
+
+export const {
+    useGetNotesQuery
+} = notesApliSlice;
+
+
+export const selectNotesResult = notesApliSlice.endpoints.getNotes.select();
+
+const selectNotesData = createSelector(
+    selectNotesResult,
+    notesResult => notesResult.data
+)
+
+export const {
+    selectAll: selectAllnotes,
+    selectById: selectNoteById,
+    selectIds: selectNoteIds
+} = notesAdapter.getSelectors(state => selectNotesData(state) ?? initialState)
